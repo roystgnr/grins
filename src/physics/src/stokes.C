e1cbb8bc9f87513bf07a38110c9037266fe0d251
Roy H. Stogner <roystgnr@ices.utexas.edu>
2014-10-17 17:28:10 -0500

    Compatibility with libMesh interior_rate API

--- src/physics/src/stokes.C
+++ src/physics/src/stokes.C
@@ -335,16 +335,15 @@ namespace GRINS
       {
         // For the mass residual, we need to be a little careful.
         // The time integrator is handling the time-discretization
-        // for us so we need to supply M(u_fixed)*u for the residual.
-        // u_fixed will be given by the fixed_interior_* functions
-        // while u will be given by the interior_* functions.
-        libMesh::Real u_dot = context.interior_value(_flow_vars.u_var(), qp);
-        libMesh::Real v_dot = context.interior_value(_flow_vars.v_var(), qp);
-
-        libMesh::Real w_dot = 0.0;
+        // for us so we need to supply M(u_fixed)*u' for the residual.
+        // u_fixed will be given by the fixed_interior_value function
+        // while u' will be given by the interior_rate function.
+        libMesh::Real u_dot, v_dot, w_dot = 0.0;
+        context.interior_rate(_flow_vars.u_var(), qp, u_dot);
+        context.interior_rate(_flow_vars.v_var(), qp, v_dot);
 
         if( _dim == 3 )
-          w_dot = context.interior_value(_flow_vars.w_var(), qp);
+          context.interior_rate(_flow_vars.w_var(), qp, w_dot);
       
         for (unsigned int i = 0; i != n_u_dofs; ++i)
           {
